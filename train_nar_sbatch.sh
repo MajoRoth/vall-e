05b8c87cc1dd1d90e728d507c849e9dde3a8688b
amitroth <amitroth@gmail.com>
2023-05-22 15:57:00 +0300

    config

---
+++ train_nar_sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --mem=32
+#SBATCH --time=2-0
+#SBATCH --gres=gpu:1,vmem:16g
+#SBATCH --exclude=gsm-04
+
+dir=/cs/labs/adiyoss/amitroth/vall-e
+cd $dir
+
+conda activate vall-e
+module load cuda/11.7
+module load cudnn
+
+python -m vall_e.train yaml=config/saspeech/nar.yml
+
