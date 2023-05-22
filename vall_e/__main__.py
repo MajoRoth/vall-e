da7167e241b4a996fd71ebe47ac04bf0c283679d
amitroth <amitroth@gmail.com>
2023-05-22 15:51:11 +0300

    args

--- vall_e/__main__.py
+++ vall_e/__main__.py
@@ -9,43 +9,43 @@ from .utils import to_device
 
 
 def main(text=None, reference=None, out_path=None, ar_ckpt=None, nar_ckpt=None, device=None):
-    parser = argparse.ArgumentParser("VALL-E TTS")
-    parser.add_argument("text")
-    parser.add_argument("reference", type=Path)
-    parser.add_argument("out_path", type=Path)
-    parser.add_argument("--ar-ckpt", type=Path, default="zoo/ar.pt")
-    parser.add_argument("--nar-ckpt", type=Path, default="zoo/nar.pt")
-    parser.add_argument("--device", default="cuda")
-    args = parser.parse_args()
+    if text is None:
+        parser = argparse.ArgumentParser("VALL-E TTS")
+        parser.add_argument("text")
+        parser.add_argument("reference", type=Path)
+        parser.add_argument("out_path", type=Path)
+        parser.add_argument("--ar-ckpt", type=Path, default="zoo/ar.pt")
+        parser.add_argument("--nar-ckpt", type=Path, default="zoo/nar.pt")
+        parser.add_argument("--device", default="cuda")
+        args = parser.parse_args()
 
-    text = args.text
-    reference = args.reference
-    out_path = args.out_path
-    ar_ckpt = args.ar_ckpt
-    nar_ckpt = args.nar_ckpt
-    device = args.device
+        text = args.text
+        reference = args.reference
+        out_path = args.out_path
+        ar_ckpt = args.ar_ckpt
+        nar_ckpt = args.nar_ckpt
+        device = args.device
 
-    print(args)
 
-    ar = torch.load(args.ar_ckpt).to(args.device)
-    nar = torch.load(args.nar_ckpt).to(args.device)
+    ar = torch.load(ar_ckpt).to(device)
+    nar = torch.load(nar_ckpt).to(device)
 
     symmap = ar.phone_symmap
 
-    proms = qnt.encode_from_file(args.reference)
+    proms = qnt.encode_from_file(reference)
     proms = rearrange(proms, "1 l t -> t l")
 
-    phns = torch.tensor([symmap[p] for p in g2p.encode(args.text)])
+    phns = torch.tensor([symmap[p] for p in g2p.encode(text)])
 
-    proms = to_device(proms, args.device)
-    phns = to_device(phns, args.device)
+    proms = to_device(proms, device)
+    phns = to_device(phns, device)
 
     resp_list = ar(text_list=[phns], proms_list=[proms])
     resps_list = [r.unsqueeze(-1) for r in resp_list]
 
     resps_list = nar(text_list=[phns], proms_list=[proms], resps_list=resps_list)
-    qnt.decode_to_file(resps=resps_list[0], path=args.out_path)
-    print(args.out_path, "saved.")
+    qnt.decode_to_file(resps=resps_list[0], path=out_path)
+    print(out_path, "saved.")
 
 
 if __name__ == "__main__":
