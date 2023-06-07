0f84d0748cab778eb5dadadca035eae77d755864
amitroth <amitroth@gmail.com>
2023-06-07 22:19:14 +0300

    clean

---
+++ clean.sh
@@ -0,0 +1,11 @@
+cd /cs/labs/adiyoss/amitroth/vall-e/ckpts/saspeech
+rm -r *
+
+cd /cs/labs/adiyoss/amitroth/vall-e/runs/saspeech
+rm -r *
+
+cd /cs/labs/adiyoss/amitroth/vall-e/runs/wavs
+rm -r *
+
+cd /cs/labs/adiyoss/amitroth/vall-e
+rm slurm-*
