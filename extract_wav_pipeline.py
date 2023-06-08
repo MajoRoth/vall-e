a3c32da048ec2106a7c316a64d31cd79b48eb6de
amitroth <amitroth@gmail.com>
2023-06-08 09:30:25 +0300

    extract wav pipepline

--- extract_wav_pipeline.py
+++ extract_wav_pipeline.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import torchaudio
 from torch.utils.tensorboard import SummaryWriter
@@ -6,51 +7,76 @@ from torch.utils.tensorboard import SummaryWriter
 from vall_e.export import main as main_export
 from vall_e.__main__ import main as main_test
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+LATEST_PATH_AR = "/cs/labds/adiyoss/amitroth/vall-e/ckpts/saspeech/ar/model/lates"
+LATEST_PATH_NAR = "/cs/labds/adiyoss/amitroth/vall-e/ckpts/saspeech/"
+
 
 def main():
+    now = datetime.now()
+    FORMAT = "%d-%m-%Y-%H:%M"
+    _writer = SummaryWriter(log_dir=f"runs/wavs_{now.strftime(FORMAT)}")
 
-    _writer = SummaryWriter(log_dir=f"runs/wavs")
-
-
-    """
-        Can only export the current model ckpt due to config file...
-    """
-    os.system("python -m vall_e.export zoo/saspeech/ar.pt yaml=config/saspeech/ar.yml")
-    print(bcolors.OKGREEN + "EXPORTED AR" + bcolors.ENDC)
-
-    os.system("python -m vall_e.export zoo/saspeech/nar.pt yaml=config/saspeech/nar.yml")
-    print(bcolors.OKGREEN + "EXPORTED AR" + bcolors.ENDC)
-
-    sentence = "היי, זה משפט לדוגמא כדי שאני אשמע אם המודל מדבר טוב"
+    latest_ar = None
+    new_ar_ckpt = False
+    latest_nar = None
+    new_nar_ckpt = False
 
 
-    main_test(text=sentence,
-         reference="/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/reference.wav",
-         out_path=f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_test.wav",
-         ar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/saspeech/ar.pt",
-         nar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/saspeech/nar.pt",
-         device="cuda")
+    while True:
 
-    print(bcolors.OKGREEN + "CREATED WAV" + bcolors.ENDC)
+        if os.path.isfile(LATEST_PATH_AR):  # Check for new ar ckpt
+            f = open(LATEST_PATH_AR, 'r')
+            latest_ar_current_value = f.read()
+
+            if latest_ar_current_value != latest_ar:
+                os.system("python -m vall_e.export zoo/saspeech/ar.pt yaml=config/saspeech/ar.yml")
+                print(f"EXPORTED AR {latest_ar_current_value}")
+
+                latest_ar = latest_ar_current_value
+                new_ar_ckpt = True
+
+        if os.path.isfile(LATEST_PATH_NAR):  # Check for new nar ckpt
+            f = open(LATEST_PATH_NAR, 'r')
+            latest_nar_current_value = f.read()
+
+            if latest_nar_current_value != latest_nar:
+                os.system("python -m vall_e.export zoo/saspeech/nar.pt yaml=config/saspeech/nar.yml")
+                print(f"EXPORTED NAR {latest_nar_current_value}")
+
+                latest_nar = latest_nar_current_value
+                new_nar_ckpt = True
 
 
-    wav, sr = torchaudio.load(f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_test.wav")
-    print(bcolors.OKGREEN + "LOADED WAV" + bcolors.ENDC)
-    _writer.add_audio(tag=f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_test.wav", snd_tensor=wav, sample_rate=sr)
-    print(bcolors.OKGREEN + "SENT TO TENSOR BOARD" + bcolors.ENDC)
+        if new_ar_ckpt and new_nar_ckpt:  # Create wav from 2 new ckpts
+            new_ar_ckpt = False
+            new_nar_ckpt = False
+
+            try:
+                ckpt_number = int(latest_ar.split('_')[1])
+            except Exception as e:
+                print(e)
+                ckpt_number = 99999
+
+            sentence = "היי, זה משפט לדוגמא כדי שאני אשמע אם המודל מדבר טוב"
+
+            main_test(text=sentence,
+                      reference="/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/reference.wav",
+                      out_path=f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_{ckpt_number}.wav",
+                      ar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/saspeech/ar.pt",
+                      nar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/saspeech/nar.pt",
+                      device="cuda")
+
+            print("CREATED WAV test_{ckpt_number}")
+            wav, sr = torchaudio.load(f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_{ckpt_number}.wav")
+            print("LOADED WAV test_{ckpt_number}")
+            _writer.add_audio(tag=f"/cs/labs/adiyoss/amitroth/vall-e/output/saspeech/test_{ckpt_number}.wav", snd_tensor=wav,
+                              sample_rate=sr)
+            print("SENT TO TENSOR BOARD")
 
 
-    print("SENT TO WRITER")
+
+
+
 
 if __name__ == "__main__":
     main()
