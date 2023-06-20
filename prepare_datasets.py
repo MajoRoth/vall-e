5f12d93ed1c5595b2a4874269f9399a5929ffe41
amitroth <amitroth@gmail.com>
2023-06-20 18:33:12 +0300

    debug

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -75,6 +75,7 @@ class Dataset:
                 print(chunk_array)
                 print(type(chunk_array))
                 chunk_tensor = torch.from_numpy(chunk_array)
+                chunk_tensor = chunk_tensor.T
 
                 chunk.export(
                         os.path.join("/cs/labs/adiyoss/amitroth/vall-e", f"{self.name}-{i}-{j}.wav"),
