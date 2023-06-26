f198c579175c4453fc8f92b1a3dbb91bdf77e008
amitroth <amitroth@gmail.com>
2023-06-26 16:07:46 +0300

    multi process

--- transcribe_osim_history.sh
+++ transcribe_osim_history.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=train_ar
-#SBATCH -c 16
-#SBATCH --mem=32g
+#SBATCH --job-name=whisper
+#SBATCH -c 8
+#SBATCH --mem=16g
 #SBATCH --time=3-0
-#SBATCH --gres=gpu:1,vmem:24g
+#SBATCH --gres=gpu:1,vmem:16g
 #SBATCH --exclude=gsm-04
 
 dir=/cs/labs/adiyoss/amitroth/vall-e
@@ -18,5 +18,5 @@ python --version
 module load cuda/11.7
 module load cudnn
 
-python prepare_datasets.py transcribe osim-history
+python prepare_datasets.py transcribe osim-history ${SLURM_ARRAY_TASK_ID} 30
 
