980d73b0b2c003cda38ec1f60c9b366cf78b72cb
amitroth <amitroth@gmail.com>
2023-07-10 17:39:46 +0300

    sbatch

--- sbatch/transcribe_geekonomy.sh
+++ sbatch/transcribe_geekonomy.sh
@@ -3,9 +3,9 @@
 #SBATCH -c4
 #SBATCH --mem=16g
 #SBATCH --time=7-0
-#SBATCH --gres=gpu:1,vmem:10g
+#SBATCH --gres=gpu:1,vmem:8g
 #SBATCH --exclude=gsm-04
-#SBATCH --array=1-50
+#SBATCH --array=1-341
 #SBATCH --output=/cs/labs/adiyoss/amitroth/vall-e/slurm_outputs/%A_%a_%x.out
 
 dir=/cs/labs/adiyoss/amitroth/vall-e
@@ -20,5 +20,5 @@ python --version
 module load cuda/11.7
 module load cudnn
 
-python prepare_datasets.py transcribe geekonomy ${SLURM_ARRAY_TASK_ID} 50
+python prepare_datasets.py transcribe geekonomy ${SLURM_ARRAY_TASK_ID} 341
 
