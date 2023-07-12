5d5a3da10249ac717a42fb0aebba78e01a117604
amitroth <amitroth@gmail.com>
2023-07-12 16:01:00 +0300

    wav

--- extract_wav_pipeline.py
+++ extract_wav_pipeline.py
@@ -12,12 +12,10 @@ from vall_e.__main__ import main as main_test
 
 
 
-def main():
-    now = datetime.now()
-    FORMAT = "%d-%m-%Y-%H:%M"
-    _writer = SummaryWriter(log_dir=f"runs/wavs_{now.strftime(FORMAT)}")
+def ckpt_pipeline(writer, cfg):
 
-    yaml_cfg = OmegaConf.load("config/hebrew/config.yml")
+
+
 
     latest_ar = None
     new_ar_ckpt = False
@@ -25,12 +23,12 @@ def main():
     new_nar_ckpt = False
     print("Starting To Extract")
     while True:
-        if os.path.isfile(yaml_cfg.latest_ar_path):  # Check for new ar ckpt
-            f = open(yaml_cfg.latest_ar_path, 'r')
+        if os.path.isfile(cfg.latest_ar_path):  # Check for new ar ckpt
+            f = open(cfg.latest_ar_path, 'r')
             latest_ar_current_value = f.read()
 
             if latest_ar_current_value != latest_ar:
-                os.system(f"python -m vall_e.export {yaml_cfg.ar_ckpt_path} yaml=config/hebrew/ar.yml")
+                os.system(f"python -m vall_e.export {cfg.ar_ckpt_path} yaml=config/hebrew/ar.yml")
                 print(" ")
                 print(f"--- EXPORTED AR {latest_ar_current_value}")
                 print(" ")
@@ -39,17 +37,17 @@ def main():
                 new_ar_ckpt = True
         else:
             print(" ")
-            print(f"path does not exist {yaml_cfg.latest_ar_path}")
+            print(f"path does not exist {cfg.latest_ar_path}")
             print(" ")
 
 
 
-        if os.path.isfile(yaml_cfg.latest_nar_path):  # Check for new nar ckpt
-            f = open(yaml_cfg.latest_nar_path, 'r')
+        if os.path.isfile(cfg.latest_nar_path):  # Check for new nar ckpt
+            f = open(cfg.latest_nar_path, 'r')
             latest_nar_current_value = f.read()
 
             if latest_nar_current_value != latest_nar:
-                os.system(f"python -m vall_e.export {yaml_cfg.nar_ckpt_path} yaml=config/hebrew/nar.yml")
+                os.system(f"python -m vall_e.export {cfg.nar_ckpt_path} yaml=config/hebrew/nar.yml")
                 print(" ")
                 print(f"--- EXPORTED NAR {latest_nar_current_value}")
                 print(" ")
@@ -59,7 +57,7 @@ def main():
         else:
             print(" ")
 
-            print(f"path does not exist {yaml_cfg.latest_ar_path}")
+            print(f"path does not exist {cfg.latest_ar_path}")
             print(" ")
 
 
@@ -78,11 +76,11 @@ def main():
 
             sentence = "היי, זה משפט לדוגמא כדי שאני אשמע אם המודל מדבר טוב"
 
-            for ref in [yaml_cfg.reference_path_hayot, yaml_cfg.reference_path_amit, yaml_cfg.reference_path_shahar]:
+            for ref in [cfg.reference_path_hayot, cfg.reference_path_amit, cfg.reference_path_shahar]:
                 file_name = ref.split("/")[-1]
                 file_name_without_wav = file_name.split(".")[0]
                 wav_name = f"test_{file_name_without_wav}_{ckpt_name}.wav"
-                output_path = f"{yaml_cfg.output_path}/{wav_name}"
+                output_path = f"{cfg.output_path}/{wav_name}"
 
                 main_test(text=sentence,
                           reference=ref,
@@ -96,7 +94,7 @@ def main():
                 wav, sr = torchaudio.load(output_path)
 
                 print(f"LOADED WAV {wav_name}")
-                _writer.add_audio(tag=f"{wav_name}", snd_tensor=wav,
+                writer.add_audio(tag=f"{wav_name}", snd_tensor=wav,
                                   sample_rate=sr)
 
                 print("SENT TO TENSOR BOARD")
@@ -104,7 +102,39 @@ def main():
 
 
 
+def upload_wav(writer, cfg):
+    sentence = "היי, זה משפט לדוגמא כדי שאני אשמע אם המודל מדבר טוב"
+
+    ckpt_name = "test"
+
+    for ref in [cfg.reference_path_hayot, cfg.reference_path_amit, cfg.reference_path_shahar]:
+        file_name = ref.split("/")[-1]
+        file_name_without_wav = file_name.split(".")[0]
+        wav_name = f"test_{file_name_without_wav}_{ckpt_name}.wav"
+        output_path = f"{cfg.output_path}/{wav_name}"
+
+        main_test(text=sentence,
+                  reference=ref,
+                  out_path=output_path,
+                  ar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/hebrew/ar.pt",
+                  nar_ckpt="/cs/labs/adiyoss/amitroth/vall-e/zoo/hebrew/nar.pt",
+                  device="cuda")
+
+        print(f"CREATED WAV test_{file_name_without_wav}_{ckpt_name}")
+        wav, sr = torchaudio.load(output_path)
+
+        print(f"LOADED WAV {wav_name}")
+        writer.add_audio(tag=f"{wav_name}", snd_tensor=wav,
+                          sample_rate=sr)
+
+        print("SENT TO TENSOR BOARD")
 
 
 if __name__ == "__main__":
-    main()
+    now = datetime.now()
+    FORMAT = "%d-%m-%Y-%H:%M"
+    _writer = SummaryWriter(log_dir=f"runs/wavs_{now.strftime(FORMAT)}")
+
+    yaml_cfg = OmegaConf.load("config/hebrew/config.yml")
+
+    upload_wav(_writer, yaml_cfg)
